a7032ed22734ce4a035cafc239b1778934603744
Massiré Touré <massire@Massires-MacBook-Air.local>
2023-07-04 16:39:49 +0200

    swagger initialization

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,39 @@
 import express from "express"
-
 import cookieParser from "cookie-parser"
-
 import 'dotenv/config'
 import config from "./src/config.js"
+
+import swaggerAutogen from 'swagger-autogen';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJson from './src/utils/swagger/swagger-output.json' assert { type: "json" };
 
 import {getFirebase, initAdminFirebase, initFirebase} from "./src/database/firebase.js"
 
 import AuthRoute from "./src/routes/AuthRoute.js"
 import EstateRoute from "./src/routes/EstateRoute.js"
 
+const doc = {
+  info: {
+    title: 'API',
+    description: 'Endpoints for the API',
+  },
+  host: 'localhost:3001',
+  schemes: ['http'],
+};
+
+const outputFile = './src/utils/swagger/swagger-output.json';
+const endpointsFiles = ['./src/routes/AuthRoute.js', './src/routes/EstateRoute.js'];
+
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
 
 const app = express();
 app.use(express.json())
 app.use(cookieParser())
+app.use('/api-docs', swaggerUi.serve);
+app.get('/api-docs', swaggerUi.setup(swaggerJson));
+
+
 initFirebase()
 initAdminFirebase()
 
